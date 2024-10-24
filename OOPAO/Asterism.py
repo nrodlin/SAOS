Coordinates input for NGS, Asterism and Sun are in degrees. Corrected errors inside Telescope, Asterism and Atmosphere classes in which the angle was treated as in radians.
Partial support for the SUN inside the classes

OOPAO/Asterism.py
@@ -41,13 +41,13 @@ class Asterism:
         src_3 = Source(opticalBand = 'H', magnitude = 8, coordinates=[60,120])
         src_4 = Source(opticalBand = 'H', magnitude = 8, coordinates=[60,240])
         
-        ast = Asterism([src_1,src_2,src_3, src_4])
+        ast = Asterism([src_1,src_2,src_3, src_4]
 
         
         """
         self.n_source = len(list_src)
         self.src = list_src
-        self.coordinates = []
+        self.coordinates = [] # polar, [r, theta] where theta is in [deg]
 
         self.altitude = []
         self.nPhoton = 0
@@ -83,10 +83,23 @@ class Asterism:
             tmp_OPD = telescope.OPD_no_pupil.copy()
             telescope.OPD_no_pupil = [tmp_OPD for i in range(self.n_source)]
         for i in range(self.n_source):
+            telescope.OPD = telescope.OPD*telescope.pupil # here to ensure that a new pupil is taken into account
+
             # update the phase of the source
             self.src[i].phase = telescope.OPD[i]*2*np.pi/self.src[i].wavelength
             self.src[i].phase_no_pupil = telescope.OPD_no_pupil[i]*2*np.pi/self.src[i].wavelength
-            
+    
+            # compute the variance in the pupil
+            self.src[i].var        = np.var(self.src[i].phase[np.where(telescope.pupil==1)])
+            # assign the source object to the obj object
+    
+            self.src[i].fluxMap    = telescope.pupilReflectivity*self.src[i].nPhoton*telescope.samplingTime*(telescope.D/telescope.resolution)**2
+            if telescope.optical_path is None:
+                telescope.optical_path = []
+                telescope.optical_path.append([self.src[i].type + '('+self.src[i].optBand+')',id(self)])
+                telescope.optical_path.append([telescope.tag,id(telescope)])
+            else:
+                telescope.optical_path[0] =[self.type + '('+self.src[i].optBand+')',id(self)]           
         # assign the source object to the telescope object
         telescope.src   = self
         

OOPAO/Atmosphere.py
@@ -225,7 +225,7 @@ class Atmosphere:
         layer.center = layer.resolution//2
         
         if self.asterism is None:
-            [x_z,y_z] = pol2cart(layer.altitude*np.tan(self.telescope.src.coordinates[0]/206265) * layer.resolution / layer.D,np.deg2rad(self.telescope.src.coordinates[1]))
+            [x_z,y_z] = pol2cart(layer.altitude*np.tan(np.deg2rad(self.telescope.src.coordinates[0])/206265) * layer.resolution / layer.D, np.deg2rad(self.telescope.src.coordinates[1]))
 
             center_x = int(y_z)+layer.resolution//2
             center_y = int(x_z)+layer.resolution//2
@@ -237,7 +237,7 @@ class Atmosphere:
             layer.extra_sx      = []
             layer.extra_sy      = []
             for i in range(self.asterism.n_source):
-                 [x_z,y_z] = pol2cart(layer.altitude*np.tan(self.telescope.src.coordinates[i][0]/206265) * layer.resolution / layer.D,np.deg2rad(self.asterism.coordinates[i][1]))
+                 [x_z,y_z] = pol2cart(layer.altitude*np.tan(np.deg2rad(self.asterism.coordinates[i][0])/206265) * layer.resolution / layer.D,np.deg2rad(self.asterism.coordinates[i][1]))
                  layer.extra_sx.append(int(x_z)-x_z)
                  layer.extra_sy.append( int(y_z)-y_z)
                  center_x = int(y_z)+layer.resolution//2
@@ -327,7 +327,7 @@ class Atmosphere:
                         raise ValueError('The chromatic_shift property is expected to be the same length as the number of atmospheric layer. ')    
                 else:
                     chromatic_shift = 0
-                [x_z,y_z] = pol2cart(layer.altitude*np.tan((self.telescope.src.coordinates[0]+chromatic_shift)/206265) * layer.resolution / layer.D,np.deg2rad(self.telescope.src.coordinates[1]))
+                [x_z,y_z] = pol2cart(layer.altitude*np.tan(np.deg2rad(self.telescope.src.coordinates[0]+chromatic_shift)/206265) * layer.resolution / layer.D,np.deg2rad(self.telescope.src.coordinates[1]))
                 layer.extra_sx = int(x_z)-x_z
                 layer.extra_sy = int(y_z)-y_z
                 
@@ -341,7 +341,7 @@ class Atmosphere:
                 layer.extra_sx = []
                 layer.extra_sy = []
                 for i in range(self.asterism.n_source):
-                    [x_z,y_z] = pol2cart(layer.altitude*np.tan(self.asterism.coordinates[i][0]/206265) * layer.resolution / layer.D,np.deg2rad(self.asterism.coordinates[i][1]))
+                    [x_z,y_z] = pol2cart(layer.altitude*np.tan(np.deg2rad(self.asterism.coordinates[i][0])/206265) * layer.resolution / layer.D,np.deg2rad(self.asterism.coordinates[i][1]))
                     layer.extra_sx.append(int(x_z)-x_z)
                     layer.extra_sy.append( int(y_z)-y_z)
                     center_x = int(y_z)+layer.resolution//2
@@ -702,7 +702,7 @@ class Atmosphere:
         normalized_speed = np.asarray(self.windSpeed)/max(self.windSpeed)
 
         if self.telescope.src.tag =='asterism':
-            list_src = self.telescope.src.src
+            list_src = self.asterism
         
         plt.figure(fig_index,figsize = [n_sp*4,3*(1+display_cn2)], edgecolor = None)
         if display_cn2:
@@ -756,7 +756,7 @@ class Atmosphere:
                     else:
                         chromatic_shift = 0
                     
-                    [x_z,y_z] = pol2cart(tmpLayer.altitude*np.tan((list_src[i_source].coordinates[0]+chromatic_shift)/206265) ,np.deg2rad(list_src[i_source].coordinates[1]))
+                    [x_z,y_z] = pol2cart(tmpLayer.altitude*np.tan(np.deg2rad(list_src[i_source].coordinates[0]+chromatic_shift)/206265) ,np.deg2rad(list_src[i_source].coordinates[1]))
                     center = 0
                     [x_c,y_c] = pol2cart(tmpLayer.D_fov/2, np.linspace(0,2*np.pi,100,endpoint=True))  
                     nm = (list_src[i_source].type) +'@'+str(list_src[i_source].coordinates[0])+'"'
@@ -783,7 +783,7 @@ class Atmosphere:
                     chromatic_shift = 0
                     
                     
-                [x_z,y_z] = pol2cart((self.telescope.src.coordinates[0]+chromatic_shift)*np.tan((self.telescope.src.coordinates[0]+chromatic_shift)/206265) * tmpLayer.resolution / tmpLayer.D,np.deg2rad(self.telescope.src.coordinates[1]))
+                [x_z,y_z] = pol2cart((self.telescope.src.coordinates[0]+chromatic_shift)*np.tan(np.deg2rad(self.telescope.src.coordinates[0]+chromatic_shift)/206265) * tmpLayer.resolution / tmpLayer.D,np.deg2rad(self.telescope.src.coordinates[1]))
             
                 center = 0
                 [x_c,y_c] = pol2cart(tmpLayer.D_fov/2, np.linspace(0,2*np.pi,100,endpoint=True))  

OOPAO/ExtendedSource.py
@@ -117,7 +117,7 @@ class ExtendedSource(Source):
             raise BaseException("Too many directions, processing will not be feasible")
         
         self.subDirs_coordinates, self.subDirs_sun = self.get_subDirs() # Coordinates are in polar [r, theta(radians)] + FoV [arcsec]
-                                                                        # The origin is the center of the Subaperture.
+                                                                        # The origin is the telescope axis.
 
         # Finally, create an Asterism using the coordinates of the subdirections to monitor the atmosphere in those lines of sight
 
@@ -182,8 +182,8 @@ class ExtendedSource(Source):
     def load_sun_img(self):
         tmp_sun = fits.open(self.img_path)[0].data
 
-        cx = (self.coordinates[0] * np.cos(self.coordinates[1] * (180/np.pi)) / self.img_PS) + tmp_sun.shape[0]//2
-        cy = (self.coordinates[0] * np.sin(self.coordinates[1] * (180/np.pi)) / self.img_PS) + tmp_sun.shape[1]//2
+        cx = (self.coordinates[0] * np.cos(np.deg2rad(self.coordinates[1])) / self.img_PS) + tmp_sun.shape[0]//2
+        cy = (self.coordinates[0] * np.sin(np.deg2rad(self.coordinates[1])) / self.img_PS) + tmp_sun.shape[1]//2
 
         width_subap_nopad = self.fov / self.img_PS
         width_subap_pad = (self.fov+self.patch_padding) / self.img_PS
@@ -203,18 +203,21 @@ class ExtendedSource(Source):
             subDir_size = self.fov+self.patch_padding
         
         subDir_imgs = np.zeros((np.ceil(subDir_size/self.img_PS).astype(int), np.ceil(subDir_size/self.img_PS).astype(int), self.nSubDirs, self.nSubDirs))
-
+        
+        tel_x = self.coordinates[0] * np.cos(np.deg2rad(self.coordinates[1]))
+        tel_y = self.coordinates[0] * np.sin(np.deg2rad(self.coordinates[1]))
+        
         for dirX in range(self.nSubDirs):
             for dirY in range(self.nSubDirs):
                 # Define subDir origin in the mid of the subDir + displace to iterate through the subDirs + 
                 # (-Translation) to define the global axis in the centre of the subaperture
                 dirx_c = (subDir_size/2) + dirX * (subDir_size/2) +  - (self.fov+self.patch_padding)/2
                 diry_c = (subDir_size/2) + dirY * (subDir_size/2) +  - (self.fov+self.patch_padding)/2
-                  
-                # Now, change to polar
 
-                subDir_loc[0, dirX, dirY] = np.sqrt(dirx_c**2 + diry_c**2)
-                subDir_loc[1, dirX, dirY] = np.arctan2(diry_c, dirx_c) * (180/np.pi) # degrees
+                # Now, change to polar, moving the origin towards the telescope axis
+
+                subDir_loc[0, dirX, dirY] = np.sqrt((dirx_c+tel_x)**2 + (diry_c+tel_y)**2)
+                subDir_loc[1, dirX, dirY] = np.rad2deg(np.arctan2((diry_c+tel_y), (dirx_c+tel_x)))
                 subDir_loc[2, dirX, dirY] = subDir_size
 
                 # Finally, crop the region

OOPAO/Source.py
@@ -34,7 +34,7 @@ class Source:
         magnitude : float
             The magnitude of the star.
         coordinates : list, optional
-            DESCRIPTION. The default is [0,0].
+            DESCRIPTION. The default is [0,0], in [deg]
         altitude : float, optional
             DESCRIPTION. The default is np.inf.
         laser_coordinates : list, optional

OOPAO/Telescope.py
@@ -215,16 +215,6 @@ class Telescope:
             
         elif self.src.tag == 'asterism':
             input_source = self.src.src
-            r = np.squeeze(np.asarray(self.src.coordinates))[:,0]
-            theta = np.squeeze(np.asarray(self.src.coordinates))[:,1]
-            
-            x_max = max(np.abs(r * np.cos(theta)))
-            y_max = max(np.abs(r * np.sin(theta)))
-            
-            if max(x_max,y_max) > max(self.xPSF_arcsec):
-                factor = 0
-            else:
-                factor = 1 
         else:
             input_source = [self.src]
 
@@ -245,12 +235,12 @@ class Telescope:
             else:
                 amp_mask = self.amplitude_filtered               
                 phase    = self.phase_filtered
-    
+
             amp      = amp_mask*self.pupil*self.pupilReflectivity*np.sqrt(input_source[i_src].fluxMap)
             
             # add a Tip/Tilt for off-axis sources
-            [Tip,Tilt]            = np.meshgrid(np.linspace(-np.pi,np.pi,self.resolution),np.linspace(-np.pi,np.pi,self.resolution))               
-            self.delta_TT = input_source[i_src].coordinates[0]*(1/conversion_constant)*(self.D/input_source[i_src].wavelength)*(np.cos(input_source[i_src].coordinates[1])*Tip+np.sin(input_source[i_src].coordinates[1])*Tilt)*self.pupil
+            [Tip,Tilt]            = np.meshgrid(np.linspace(-np.pi,np.pi,self.resolution),np.linspace(-np.pi,np.pi,self.resolution))            
+            self.delta_TT = input_source[i_src].coordinates[0]*(1/conversion_constant)*(self.D/input_source[i_src].wavelength)*(np.cos(np.deg2rad(input_source[i_src].coordinates[1]))*Tip+np.sin(np.deg2rad(input_source[i_src].coordinates[1]))*Tilt)*self.pupil
             
             # axis in arcsec
             self.xPSF_arcsec       = [-conversion_constant*(input_source[i_src].wavelength/self.D) * (img_resolution/2/zeroPaddingFactor), conversion_constant*(input_source[i_src].wavelength/self.D) * (img_resolution/2/zeroPaddingFactor)]
@@ -277,10 +267,8 @@ class Telescope:
             output_PSF_norma = output_PSF_norma[0]
 
         self.PSF        = output_PSF.copy()
-        self.PSF_norma  = output_PSF_norma.copy()                 
-  
- 
-    
+        self.PSF_norma  = output_PSF_norma.copy()     
+
     def PropagateField(self, amplitude, phase, zeroPaddingFactor, img_resolution = None):
 
         xp                  = np
@@ -503,7 +491,7 @@ class Telescope:
                     for i_src in range(len(input_source)):
                         obj.telescope.src         = input_source[i_src]
                         [Tip,Tilt]                = np.meshgrid(np.linspace(-np.pi,np.pi,self.resolution),np.linspace(-np.pi,np.pi,self.resolution))               
-                        delta_TT                  = input_source[i_src].coordinates[0]*(1/((180/np.pi)*3600))*(self.D/input_source[i_src].wavelength)*(np.cos(input_source[i_src].coordinates[1])*Tip+np.sin(input_source[i_src].coordinates[1])*Tilt)*self.pupil
+                        delta_TT                  = input_source[i_src].coordinates[0]*(1/((180/np.pi)*3600))*(self.D/input_source[i_src].wavelength)*(np.cos(np.deg2rad(input_source[i_src].coordinates[1]))*Tip+np.sin(np.deg2rad(input_source[i_src].coordinates[1]))*Tilt)*self.pupil
                         obj.wfs_measure(phase_in  = input_source[i_src].phase + delta_TT ,integrate = False )
                         output_raw_data += obj.raw_data.copy()
                         if obj.tag=='pyramid':
@@ -523,7 +511,7 @@ class Telescope:
                     self.optical_path.append([obj.tag,id(obj)])
 
                 self.computePSF(detector = obj)
-                obj.fov_arcsec = self.xPSF_arcsec[1] -self.xPSF_arcsec[0] 
+                obj.fov_arcsec = self.xPSF_arcsec[1] - self.xPSF_arcsec[0] 
                 obj.fov_rad = self.xPSF_rad[1] - self.xPSF_rad[0]
                 if obj.integrationTime is not None:
                     if obj.integrationTime < self.samplingTime:
@@ -561,7 +549,6 @@ class Telescope:
                 FP_filtered         = FP_in*np.fft.fftshift(obj.mask)
                 em_field            = np.fft.ifft2(FP_filtered)
                 self.em_field_filtered  = em_field[obj.center-self.resolution//2:obj.center+self.resolution//2,obj.center-self.resolution//2:obj.center+self.resolution//2]
-                # self.phase_filtered = np.arctan2(np.imag(self.em_field_filtered),np.real(self.em_field_filtered))*self.pupil
                 self.phase_filtered = ((np.angle(self.em_field_filtered)))*self.pupil
                 
                 self.amplitude_filtered  = np.abs(self.em_field_filtered)