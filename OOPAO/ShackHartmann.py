810a7fcf4650c355746db85daad32c67b5dacdcc
nrodlin <nicolas.linares@iac.es>
2024-11-21 11:35:39 +0000

    Solved minor error regarding the criteria to use multi-frame EM field computation or single frame

--- OOPAO/ShackHartmann.py
+++ OOPAO/ShackHartmann.py
@@ -330,6 +330,7 @@ class ShackHartmann:
     def get_lenslet_em_field(self,phase):
         tmp_phase_h_split = np.hsplit(phase.T,self.nSubap)
         self.cube_em = np.zeros([self.nSubap**2,self.n_pix_lenslet_init,self.n_pix_lenslet_init],dtype=complex)
+        print(phase.shape, np.asarray(tmp_phase_h_split).shape, self.cube_em.shape)
         for i in range(self.nSubap):
             tmp_phase_v_split = np.vsplit(tmp_phase_h_split[i],self.nSubap)
             self.cube_em[i*self.nSubap:(i+1)*self.nSubap,self.center_init - self.n_pix_subap_init//2:self.center_init+self.n_pix_subap_init//2,self.center_init - self.n_pix_subap_init//2:self.center_init+self.n_pix_subap_init//2] = np.exp(1j*np.asarray(tmp_phase_v_split))
@@ -541,7 +542,7 @@ class ShackHartmann:
             
         if self.is_geometric is False:
             ##%%%%%%%%%%%%  DIFFRACTIVE SH WFS %%%%%%%%%%%%
-            if np.ndim(phase_in)==2:
+            if np.ndim(self.telescope.src.phase)==2:
                 #-- case with a single wave-front to sense--
                 # reset camera frame to be filled up
                 self.raw_data   = np.zeros([self.n_pix_subap*(self.nSubap)//self.binning_factor,self.n_pix_subap*(self.nSubap)//self.binning_factor], dtype =float)
@@ -600,7 +601,7 @@ class ShackHartmann:
 
             else:
                 #-- case with multiple wave-fronts to sense--
-
+                
                 # set phase buffer
                 self.phase_buffer = np.moveaxis(self.telescope.src.phase_no_pupil,-1,0)
 
