d4c0c4d48d1842b597c016afcf5fc9a5984152c6
nrodlin <nicolas.linares@iac.es>
2026-05-25 12:45:20 +0100

    Corrected bug when the piston is included in the modal basis (whatever the reason may be)

--- SAOS/modalBasis/discHarmonicModes.py
+++ SAOS/modalBasis/discHarmonicModes.py
@@ -29,8 +29,9 @@ def generate_dh_modes(dm, nModes=None, useTorch=False, include_piston=False):
         n, m = get_index(i+1) if include_piston else get_index(i+2)
         dh_modes[:, i] = disk_harmonic_mode(m, n, r, theta)  # Apply mask
 
-        # Remove piston
-        dh_modes[dm.validAct,i] -= np.mean(dh_modes[dm.validAct,i])
+        # Remove piston from modes other than the piston mode itself
+        if not include_piston or i > 0:
+            dh_modes[dm.validAct,i] -= np.mean(dh_modes[dm.validAct,i])
 
         # Normalize energy to 1 --> Necessary before Gram-Schmidt
         energy = np.sqrt(np.mean(dh_modes[dm.validAct, i]**2))
